1bb15b51d11aa4fb26b17ec0d7298010d1dd8051
Neel Jain <neelarmstrong33@gmail.com>
2023-09-21 20:08:51 +0530

    Initial Commit

---
+++ src/Data/data.js
@@ -0,0 +1,38 @@
+export const data = {
+  personal_info: {
+    first_name: "Neel",
+    last_name: "Jain",
+    email: "neel.jain@gmail.com",
+    mobile: "+918236066632",
+    address: "202/2, New Jagdamba Colony Vikas Nagar",
+    city: "Jabalpur",
+    state: "Madhya Pradesh",
+    postal_code: "482002",
+    objective:
+      "Seeking a position to apply the knowledge that I have learned and explore abilities, and skills, towards my duties and excel in the field of FullStack Web Developer. ",
+  },
+  work_experience: [
+    {
+      id: 1,
+      job_title: "Assistant Manager",
+      organization_name: "Jio Platform Limited",
+      start_year: "2022",
+      end_year: "2022",
+    },
+    {
+      id: 2,
+      job_title: "Associate Software Developer",
+      organization_name: "Paramatrix Technologies",
+      start_year: "2021",
+      end_year: "2022",
+    },
+  ],
+  education_details: {
+    type: "Post Graduation",
+    university: "Global Engineering College",
+    degree: "B.Tech",
+    start_year: "2017",
+    end_year: "2021",
+  },
+  key_skills: ["JavaScript", "React", "MERN Stack", "Node.js", "Html"],
+};
